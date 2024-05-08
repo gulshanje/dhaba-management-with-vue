4640826cb0cae8bbee9590f66412c82ec80b517f
Gulshan <shrigulshankumar@gmail.com>
2024-05-08 22:28:32 +0300

    Added signup component

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>SignUp</h1>
+    <div class="register">
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Sign Up</button>
+
+    </div>
   </div>
 </template>
 
@@ -10,4 +17,25 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.register input{
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid skyblue;
+  cursor: pointer;
+}
+
+.register button{
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  color: #fff;
+  background-color: skyblue;
+  cursor: pointer;
+}
+</style>

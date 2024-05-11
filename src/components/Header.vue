2dd6d1210508350ebfec1dc1c35f5a5a1f195bea
Gulshan <shrigulshankumar@gmail.com>
2024-05-11 22:16:13 +0300

    Added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="nav-bar">
+        <a href="home">Home</a>
+        <a href="home">Add Dhaba</a>
+        <a href="home">Update</a>
+        <a href="home">Logout</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderPage'
+    }
+</script>
+
+<style scoped>
+body{
+    padding: 0px;
+    margin: 0px;
+}
+.nav-bar{
+    background-color: #333;
+    overflow: hidden;
+}
+.nav-bar a{
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 18px 14px;
+    text-decoration: none;
+    font-size: 18px;
+    margin-right: 5px;
+}
+.nav-bar a:hover{
+    background-color: #ddd;
+    color: #222;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
+        <HeaderPage />
         <h1>Home page view</h1>
 
     </div>
 </template>
 
 <script>
+import HeaderPage from './Header';
     export default {
         name: "HomePage",
+        components: {
+            HeaderPage
+        },
+        mounted()
+        {
+            let userInfo = localStorage.getItem('user-info')
+            if(!userInfo)
+            {
+            this.$router.push({name: 'SignUp'})
+            }
+        }
         
     }
 </script>

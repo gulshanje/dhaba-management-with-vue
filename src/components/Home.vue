e0c1d21eecc653bd9eb5d85a63afa86d9fbb10b2
Gulshan <shrigulshankumar@gmail.com>
2024-05-11 11:51:43 +0300

    Added Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Home page view</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomePage",
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
